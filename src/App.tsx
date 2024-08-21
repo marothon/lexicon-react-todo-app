ead5f9c0d572f46ffc1d0c5f351632557cfbc7d2
marothon <marothon@gmail.com>
2024-08-21 14:53:54 +0200

    Add clear completed todos and change counter to only count incomplete tasks

--- src/App.tsx
+++ src/App.tsx
@@ -9,33 +9,54 @@ export default function App() {
 
   const createTodo = (desc: string) => {
     if(desc){
-      const newTodo: Todo = {id: uuidv4(), desc: desc};
+      const newTodo: Todo = {id: uuidv4(), desc: desc, completed: false};
       setTodoList((previousTodo) => [...previousTodo, newTodo]);
       setTodoCount(todoList.length+1);
     }
   }
 
-  const removeTodo = (id: string) => {
+  const removeTodo = (todo: Todo) => {
     setTodoList((previousTodo) => {
-      return previousTodo.filter( todo => todo.id !== id);
+      return previousTodo.filter( elem => elem.id !== todo.id);
     });
-    setTodoCount(todoCount-1);
+
+    if(!todo.completed){
+      setTodoCount(todoCount-1);
+    }
+  }
+
+  const updateTodosCount = (todo: Todo) => {
+    setTodoCount(todoCount + (todo.completed ? -1 : 1));
+  }
+
+  const clearCompletedTodos = () => {
+    let completedTodos = todoList.filter( todo => todo.completed);
+    setTodoList((previousTodo) => {
+      return previousTodo.filter( todo => !todo.completed);
+    });
+    setTodoCount(todoCount-completedTodos.length);
   }
 
   return (
     <>
       <h1>{todoCount > 0 ? todoCount : null} TODO{todoCount > 0 ? 'S' : null}</h1>
-      <form onSubmit={(e) => {
+      <form className="todo-form" onSubmit={(e) => {
         e.preventDefault();
         let nextTodo = e.currentTarget.todo.value;
         e.currentTarget.todo.value = null;
         createTodo(nextTodo);
       }}>
         <input placeholder="Enter new todo" type="text" id="todo" name="todo"></input>
+        <button type="button" onClick={clearCompletedTodos}>Clear completed</button>
       </form>
       <ul className='todo-list'>
         {todoList.map( (todo, i) => 
-          (<TodoItem key={todo.id} id={todo.id} todo={todo.desc} removeTodo={removeTodo}/>)
+          (<TodoItem 
+              key={todo.id}
+              todo={todo}
+              removeTodo={removeTodo}
+              propogateCompletedTodo={updateTodosCount}
+            />)
         )}
       </ul>
     </>
@@ -44,22 +65,30 @@ export default function App() {
 
 type Todo = {
   id: string,
-  desc: string
+  desc: string,
+  completed: boolean
 }
 
 interface TodoProps {
-  id: string,
-  todo: string,
-  removeTodo: Function
+  todo: Todo,
+  removeTodo: Function,
+  propogateCompletedTodo: Function
 }
 
-function TodoItem({id, todo, removeTodo}:TodoProps){
-  const [completed, setCompleted] = useState<boolean>(false);
+function TodoItem({todo, removeTodo, propogateCompletedTodo}: TodoProps){
+  const [completed, setCompleted] = useState<boolean>(todo.completed);
+
+  const handleCompleteTodo = () => {
+    setCompleted(!completed);
+    todo.completed = !todo.completed;
+    propogateCompletedTodo(todo);
+  }
+
   return (
       <li className="todo">
-        <input type="checkbox" onClick={()=>{setCompleted(!completed)}}/>
-        <button type="button" onClick={()=>{removeTodo(id)}}>Remove</button>
-        <p className={['text', completed ? 'complete' : ''].join(' ')}>{todo}</p>
+        <input type="checkbox" onClick={handleCompleteTodo}/>
+        <button type="button" onClick={()=>{removeTodo(todo)}}>Remove</button>
+        <p className={['text', completed ? 'complete' : ''].join(' ')}>{todo.desc}</p>
       </li>
   )
 }
