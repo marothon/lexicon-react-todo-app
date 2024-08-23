f123d73006c3617b8b82777abb3c6cefdcdfde05
Max <marothon@users.noreply.github.com>
2024-08-23 15:54:03 +0200

    Solve exercise (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,65 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { v4 as uuidv4 } from 'uuid';
+import TodoItem from './components/todo-item';
+import { Todo } from './types/todo';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [todoCount, setTodoCount] = useState(0);
+  const [todoList, setTodoList] = useState<Array<Todo>>([]);
+
+
+  const createTodo = (desc: string) => {
+    if(desc){
+      const newTodo: Todo = {id: uuidv4(), desc: desc, completed: false};
+      setTodoList((previousTodo) => [...previousTodo, newTodo]);
+      setTodoCount(todoList.length+1);
+    }
+  }
+
+  const removeTodo = (todo: Todo) => {
+    setTodoList((previousTodo) => {
+      return previousTodo.filter( elem => elem.id !== todo.id);
+    });
+
+    if(!todo.completed){
+      setTodoCount(todoCount-1);
+    }
+  }
+
+  const updateTodosCount = (todo: Todo) => {
+    setTodoCount(todoCount + (todo.completed ? -1 : 1));
+  }
+
+  const clearCompletedTodos = () => {
+    setTodoList((previousTodo) => {
+      return previousTodo.filter( todo => !todo.completed);
+    });
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>{todoCount > 0 ? todoCount : null} TODO{todoCount > 0 ? 'S' : null}</h1>
+      <form className="todo-form" onSubmit={(e) => {
+        e.preventDefault();
+        let nextTodo = e.currentTarget.todo.value;
+        e.currentTarget.todo.value = null;
+        createTodo(nextTodo);
+      }}>
+        <input placeholder="Enter new todo" type="text" id="todo" name="todo"></input>
+        <button type="button" onClick={clearCompletedTodos}>Clear completed</button>
+      </form>
+      <ul className='todo-list'>
+        {todoList.map( (todo) => 
+          (<TodoItem 
+              key={todo.id}
+              todo={todo}
+              removeTodo={removeTodo}
+              propogateCompletedTodo={updateTodosCount}
+            />)
+        )}
+      </ul>
     </>
   )
 }
 
-export default App
